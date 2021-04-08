bd950ae67dc4dd16fb393c27dfd47a41b6d9dad7
Dr. Tyson McMillan <>
2021-04-08 00:29:35 +0000

    added void welcomeMessage();

--- main.cpp
+++ main.cpp
@@ -18,9 +18,11 @@ using namespace std;
 
 //$_earned       mom  dad
 double MakeBank(bool,bool); //function prototype
+void welcomeMessage(); 
 
 int main()
 {
+    welcomeMessage(); //function call 
     //function calls (true = "yes", false="no")
     cout << "\nLittle Sister Function calls: ";
     cout << "\nOption 1, Little Sister Earns: $" << MakeBank(true,false);
@@ -29,7 +31,7 @@ int main()
     cout << "\nOption 4, Little Sister Earns: $" <<  MakeBank(false,false);
 
     /*Programming Challenges
-      1. Extend this program to output how much Little Sister earns in each scenario.
+      **1. Extend this program to output how much Little Sister earns in each scenario.
       2. Create a void welcomeMessage(); function to show a nice greeting on screen to the program.
       3. Extend this program to write a void dayOfTheWeek(int); function given user input of the number (1 output Sunday);
       4. Output each day of the week in a different color.
@@ -54,8 +56,25 @@ double MakeBank(bool mom, bool dad)
    // cout << "\n\tAin't - mom" << endl; 
   }
 
+  if(dad == true)
+  {
+    amtEarned += 20.0; //amtEarned = amtEarned + 20.0; 
+  }
+
+  if(dad == false)
+  {
+     //cout << "\n\tNo - says Dad!" << endl; 
+  }
 
 	return amtEarned;
 }
 
+void welcomeMessage()
+{
+  cout << "\nMake Bank, Dr_T. Boom, Pop, Pow!!!" << endl; 
+  for(int i=0; i < 15; i++)
+  {
+    cout << "*"; 
+  }  
+}
 
