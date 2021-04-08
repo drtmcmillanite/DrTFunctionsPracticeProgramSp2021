294fa4c12a1bed2590071437b8155f7d3fce2468
Dr. Tyson McMillan <>
2021-04-08 00:09:48 +0000

    Changed comments to reflect current year.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*Folder: Little_Sister_YourLastName
 CPP: Function_Practice_Little_Sister.cpp
-Teacher: Dr. Tyson McMillan, 9-1-2019
-Student: TYPE_YOUR_NAME_HERE
+Teacher: Dr. Tyson McMillan, 04-7-2021
+Student: Dr_T
 
 Description: Dr_T's little sister is smart.
 She uses a divide and concur method to get $20 from the parents.
